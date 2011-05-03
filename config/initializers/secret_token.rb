c869abba5c290dadb57462167bf9a746b9683cba
Marco <marc.derosa@gmail.com>
2011-05-03 21:32:02 +0800

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lotl::Application.config.secret_token = 'e884194d5b5246d24254a9138d36f997975c0b894a027d70f4484d6d6067c468f1d489ffb4e38de5b36de799f6f1a4223cc74d9a39494ed6d8a8d530e6daacd3'
