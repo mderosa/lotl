740998c37f9d20e4601c1369abfae0dd83ae4d55
Marco <marc.derosa@gmail.com>
2011-07-05 20:05:17 +0800

    add helpers for task index page

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,60 @@
 module TasksHelper
+
+  # here are the valid situations that we can run into
+  # status   start_dt   release_dt  deploy_dt   term_dt   cost
+  # proposed                                              0
+  # proposed                                    1/6       0
+
+  # inprog   1/1                                          now-1
+  # inprog   1/1                                1/6       5(6-1)
+  # inprog   1/1        1/3                               3-1
+  # inprog   1/1        1/3                     1/6       5(6-1)
+  # inprog   1/1        1/5          1/4                  4-1 + 5(5-4)
+  
+  # deliv    1/1        1/3          1/4                  4-1
+  # deliv    1/1        1/5          1/4                  4-1 + 5(5-4)
+  def calc_cost(task)
+    if task.progress == 'proposed'
+      0
+    elsif task.progress == 'inProgress'
+      _calc_inProgress_cost(task)
+    elsif task.progress == 'delivered'
+      _calc_delivered_cost(task)
+    else 
+    end
+  end
+
+  # inprog   1/1                                          now-1
+  # inprog   1/1                                1/6       5(6-1)
+  # inprog   1/1        1/3                               now-1
+  # inprog   1/1        1/3                     1/6       5(6-1)
+  # inprog   1/1        1/5          1/4                  4-1 + 5(5-4)
+  def _calc_inProgress_cost(task)
+    if task.terminated_at
+      _to_days(5 * (task.terminated_at - task.work_started_at))
+    elsif task.delivered_at.nil?
+      _to_days(Time.now - task.work_started_at) 
+    else
+      _to_days(task.delivered_at - task.work_started_at + 5 * (task.work_finished_at - task.delivered_at))
+    end
+  end
+
+  # deliv    1/1        1/3          1/4                  4-1
+  # deliv    1/1        1/5          1/4                  4-1 + 5(5-4)
+  def _calc_delivered_cost(task)
+    if task.work_finished_at <= task.delivered_at
+      _to_days(task.delivered_at - task.work_started_at)
+    else
+      _to_days(task.delivered_at - task.work_started_at + 5 * (task.work_finished_at - task.delivered_at))
+    end
+  end
+
+  def _to_days(seconds)
+    (seconds/(60 * 60 * 24)).truncate
+  end
+
+  def priority_images(task)
+
+  end
+
 end
---
+++ spec/helpers/tasks_helper_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the DudesHelper. For example:
+#
+# describe DudesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe TasksHelper do
+
+  describe "the cost calculation" do
+    
+    before(:each) do
+      @params = {:progress => "proposed"}
+    end
+
+    it "should return zero for any task in 'proposed' status" do
+      task = Task.new(@params)
+      task.terminated_at = Time.utc(2000, 1, 6)
+      cost = helper.calc_cost(task)
+      cost.should eq(0)
+    end
+    
+    it ", for a normally delivered task, should calculate hours to deploy divided by 24" do
+      params = @params.merge(:progress => 'delivered')
+      task = Task.new(params)
+      task.work_started_at = Time.utc(2000, 1, 1, 1, 24)
+      task.work_finished_at = Time.utc(2000, 1, 3, 1, 33)
+      task.delivered_at = Time.utc(2000, 1, 4, 5, 21)
+      cost = helper.calc_cost(task)
+      cost.should eq(3)
+    end
+
+    it ", for a reworked, delivered task, should return the elapsed days with an additional penalty" do
+      params = @params.merge(:progress => 'delivered')
+      task = Task.new(params)
+      task.work_started_at = Time.utc(2000, 1, 1, 1, 24)
+      task.work_finished_at = Time.utc(2000, 1, 5, 1, 33)
+      task.delivered_at = Time.utc(2000, 1, 4, 5, 21)
+      cost = helper.calc_cost(task)
+      cost.should eq(7)
+    end
+
+    it "should return a straight penalty calculation for inProgress tasks with a termination date" do
+      params = @params.merge(:progress => 'inProgress')
+      task = Task.new(params)
+      task.work_started_at = Time.utc(2000, 1, 1, 1, 24)
+      task.work_finished_at = Time.utc(2000, 1, 3, 1, 33)
+      task.terminated_at = Time.utc(2000, 1, 6 , 5, 21)
+      cost = helper.calc_cost(task)
+      cost.should eq(25)
+    end
+
+    it "should return a standard days elapsed calculation when delivered_at is null and terminated_at is null" do
+      params = @params.merge(:progress => 'inProgress')
+      task = Task.new(params)
+      task.work_started_at = Time.utc(2001, 1, 1, 1, 24)
+      task.delivered_at.should be_nil
+
+      cost = helper.calc_cost(task)
+      cost.should_not be_nil
+      cost.should > 100      
+    end
+
+    it "should return elapsed days with penalty the task is inProgress and being reworked" do
+      params = @params.merge(:progress => 'inProgress')
+      task = Task.new(params)
+      task.work_started_at = Time.utc(2000, 1, 1, 1, 24)
+      task.work_finished_at = Time.utc(2000, 1, 5, 5, 22)
+      task.delivered_at = Time.utc(2000, 1, 4, 5, 21)
+
+      cost = helper.calc_cost(task)
+      cost.should_not be_nil
+      cost.should eq(8)            
+    end
+
+  end
+
+end
+
+#error = wasnt aware that I restricted access to Task through the constructor
+#error = forgot the switch parameters in unit test after a copy from another unit test
+#error = forgot to customize all code in unit test after a copy from another unit test
