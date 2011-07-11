798478849dcceecd1e2fc1bb727c8b14e89ef984
Marco <marc.derosa@gmail.com>
2011-07-11 20:03:55 +0800

    collaborators functionality working

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,9 +1,22 @@
 class CollaboratorsController < ApplicationController
 
   # POST /projects/:project_id/collaborators
-  # params = {:project_id => n, :email => m}
+  # params = {:project_id => n, :user[:email] => m}
   def create
-    respond_to do |format|
+    @user = User.find_by_email(params[:user][:email])
+    @project = Project.find(params[:project_id])
+
+    if not @user.nil?
+      if @project.users.include? @user
+        flash[:notice] = "#{@user.email} is already a collaborator"
+      else
+        @project.users << @user
+        @project.save
+        flash[:success] = 'Collaborator was added'
+      end
+      redirect_to edit_project_path(@project)
+    else
+      flash[:warning] = "not implemented"
     end
   end
 
@@ -15,3 +28,5 @@ class CollaboratorsController < ApplicationController
   end
 
 end
+
+# error: forgot to add @ before a class member
--- spec/controllers/collaborators_controller_spec.rb
+++ spec/controllers/collaborators_controller_spec.rb
@@ -1,19 +1,44 @@
 require 'spec_helper'
 
 describe CollaboratorsController do
+  
+  def login_as(user)
+    session[:user_id] = user.id
+  end
 
   describe "POST 'create'" do
-    describle "logic" do
-      it "if c E C-set                     then C-set == C-set'"
-      it "if c !E Clb-set and c E User-set then Clb-set.size + 1 = C-set'.size"
+    before(:each) do
+      @owner = Factory(:user, :active => true)
+      @proj = Factory(:project, :users => [@owner])
+      login_as(@owner)
+    end
+
+    describe "logic" do
+      it "if c E C-set                     then C-set == C-set'" do
+        post :create, :project_id => @proj.id, :user => {:email => @owner.email}
+        flash[:notice].should_not be_nil
+        assigns(:project).users.length.should eq(1)
+      end
+
+      it "if c !E Clb-set and c E User-set then Clb-set.size + 1 = C-set'.size" do
+        @collaborator = Factory(:user, :email => "collaborator@playnice.com", :active => true)
+        post :create, :project_id => @proj.id, :user => {:email => @collaborator.email}
+        flash[:success].should_not be_nil
+        assigns(:project).users.length.should eq(2)
+      end
+
       it "if c !E Clb-set and c !E Usr-set then Clb-set.size + 1 = Clb-set'.size and Usr.size + 1 = Usr'.size and email"
     end
 
     describe "page flow" do
       it "non email format => project_path w/form validation error, bad format"
-      it "if c E C-set                     => project_path w/ for validation error, already collaborator"
+      it "if c E C-set                     => project_path w/ flash notice, already collaborator"
       it "if c !E Clb-set and c E User-set => project_path w/ flash successe"
       it "if c !E Clb-set and c !E Usr-set => project_path w/ flash sucess, has been invited"
+    end
+
+    describe "security" do
+      it "should only allow project collaborators to add an delete collaborators"
     end
   end
 
@@ -21,6 +46,11 @@ describe CollaboratorsController do
     it "c !E Clb-set => project_path w/ flash error message"
     it "c E Clb-set => project_path w/ flash success message"
 
+    describe "security" do
+    end
   end
 
 end
+
+# error: did not finish entering ending ')'s
+# error: typo describe was 'describle'
