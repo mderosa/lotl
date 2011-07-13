c4e1c9d45f9e80ce1e67d6ec06c1b44a1015aed4
Marco <marc.derosa@gmail.com>
2011-07-13 21:07:30 +0800

    fix extent bug for chart

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,8 +4,10 @@ class StatisticsController < ApplicationController
 
   def index
     now = Time.now.utc
-    deliveries = Task.delivery_count_per_day(params[:project_id], now.to_date - 41, now.to_date)
-    @deliveries_per_day = fill_date_gaps(deliveries, now.to_date - 41, now.to_date)
+    first_delivery = Task.minimum("delivered_at") || now
+    from = (now.to_date - 41) < first_delivery.to_date ? first_delivery.to_date : now.to_date - 41
+    deliveries = Task.delivery_count_per_day(params[:project_id], from, now.to_date)
+    @deliveries_per_day = fill_date_gaps(deliveries, from, now.to_date)
     logger.debug("data is: #{@deliveries_per_day}")
   end
 
