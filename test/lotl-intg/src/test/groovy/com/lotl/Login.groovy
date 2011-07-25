5a56562f5afa7dbe5c8f168ad159e9fab813d053
Marco <marc.derosa@gmail.com>
2011-07-25 19:58:37 +0800

    add the start of integration tests

---
+++ test/lotl-intg/src/test/groovy/com/lotl/Login.groovy
@@ -0,0 +1,53 @@
+package com.lotl;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Login {
+	
+	@Test
+	public void testIncorrectLogin() {
+		 // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface, 
+        // not the implementation.
+        WebDriver driver = new FirefoxDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.google.com");
+
+        // Find the text input element by its name
+        def element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
+        println ("Page title is: " + driver.getTitle());
+        
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 20)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().startsWith("Cheese!");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        println("Page title is: " + driver.getTitle());
+        
+        //Close the browser
+        driver.quit();
+	}
+	
+
+}
