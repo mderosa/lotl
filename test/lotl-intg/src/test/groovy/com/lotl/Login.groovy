1a980c18e23cabf43b9e19d247febe78d20f3be9
Marco <marc.derosa@gmail.com>
2011-07-26 19:19:03 +0800

    try to make nicer buttons

--- test/lotl-intg/src/test/groovy/com/lotl/Login.groovy
+++ test/lotl-intg/src/test/groovy/com/lotl/Login.groovy
@@ -9,45 +9,24 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Login {
-	
+
 	@Test
 	public void testIncorrectLogin() {
-		 // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
-        WebDriver driver = new FirefoxDriver();
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://localhost");
+		def element = driver.findElement(By.cssSelector("div.ltl-login-form > p > a"))
+		element.click();
 
-        // Find the text input element by its name
-        def element = driver.findElement(By.name("q"));
+		(new WebDriverWait(driver, 20)).until(new ExpectedCondition<Boolean>() {
+					public Boolean apply(WebDriver d) {
+						def el = d.findElement(By.id(("user_email")))
+						return el != null;
+					}
+				});
 
-        // Enter something to search for
-        element.sendKeys("Cheese!");
-
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
-
-        // Check the title of the page
-        println ("Page title is: " + driver.getTitle());
-        
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 20)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().startsWith("Cheese!");
-            }
-        });
-
-        // Should see: "cheese! - Google Search"
-        println("Page title is: " + driver.getTitle());
-        
-        //Close the browser
-        driver.quit();
+		driver.quit();
 	}
-	
+
 
 }
