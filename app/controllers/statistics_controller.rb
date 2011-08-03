bb551f07caa887fcb0cb2cbb863c0010a7c7c784
Marco <marc.derosa@gmail.com>
2011-08-04 07:56:53 +0800

    fix statistics chart selection

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,15 +3,16 @@ class StatisticsController < ApplicationController
   include StatisticsHelper
   
   def index
-    @chart_data = fetch_delivery_count_per_day(params[:project_id], delivery_count_time_range)
+    project_id = params[:project_id]
+    @chart_data = fetch_delivery_count_per_day(project_id, delivery_count_time_range(project_id))
   end
 
   # delivery_count_time_range :: {:from => Date, :to => Date}
   # Returns up to 40 days of delivery counts if that amount of data exists for a project. This method
   # currently does not make allowances for weekends
-  def delivery_count_time_range
+  def delivery_count_time_range(project_id)
     now = Time.now.utc
-    first_delivery = Task.minimum("delivered_at") || now
+    first_delivery = Task.where("project_id = ?", project_id).minimum("delivered_at") || now
     from = (now.to_date - 41) < first_delivery.to_date ? first_delivery.to_date : now.to_date - 41
     to = now.to_date - 1
     {:from => from, :to => to}
@@ -27,8 +28,6 @@ class StatisticsController < ApplicationController
                                    expires_at = Time.new.utc.tomorrow.midnight)
     from = delivery_date_range[:from]
     to = delivery_date_range[:to]
-    puts "expires at: #{expires_at}"
-    puts "now: #{Time.new.utc}"
     data = cache.fetch "#{project_id}.delivery_count_per_day", :expires_in => (expires_at - Time.new.utc) do
       raw_delivery_data = Task.delivery_count_per_day(project_id, from, to)
       deliveries_by_day = fill_date_gaps(raw_delivery_data, from, to)
@@ -37,3 +36,5 @@ class StatisticsController < ApplicationController
   end
 
 end
+
+# error: forgot to add a where clause restricting min(delivered_at) to just project of interest
